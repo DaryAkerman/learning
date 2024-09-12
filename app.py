807cf3eba9f6c4def07662a0d4273ddf56b740b9
DaryAkerman <daryakerman2002@gmail.com>
2024-09-12 19:38:10 +0300

    Added /main to the site, add temapltes for the html code

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Welcome to my site, please sure to use /YOURNAME to get a nice page with your name."
+    return "Welcome to my site, please sure to use /YOURNAME to get a nice page with your name.\nAlso, you can do /main in order to access the best SITE ever!!"
 
 @app.route('/<name>')
 def getName(name):
     return f"Welcome! {name}, this is the best app you will see."
+
+@app.route('/main')
+def showSite():
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
